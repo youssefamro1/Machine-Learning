9e68d90cd180f6897d2a0b539717e229bbe20877
youssef amr momtaz <77958872+youssefamro1@users.noreply.github.com>
2022-05-12 08:03:19 +0200

    Add files via upload

---
+++ my_simple.py
@@ -0,0 +1,44 @@
+                
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+
+from sklearn.linear_model import LinearRegression
+regr=LinearRegression()
+regr.fit(X_train, y_train)
+
+y_pred=regr.predict(X_test)
+y_pred_train=regr.predict(X_train)
+
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train,y_pred_train,color="blue")
+plt.title('Salary vs Experience (train set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train,y_pred_train,color="blue")
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+
+
+
+
+
+
