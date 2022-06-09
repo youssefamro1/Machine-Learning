85f74d1dddb46c327d33f3b4b1d0ca0c4f2dda39
youssef amr momtaz <77958872+youssefamro1@users.noreply.github.com>
2022-06-09 22:38:01 +0200

    Update hierarchical_clustering.py

--- hierarchical_clustering.py
+++ hierarchical_clustering.py
@@ -17,7 +17,11 @@ plt.xlabel('Customers')
 plt.ylabel('Euclidean distances')
 plt.show()
 
-
+# Training the Hierarchical Clustering model on the dataset
+from sklearn.cluster import AgglomerativeClustering
+hc = AgglomerativeClustering(n_clusters = 5, affinity = 'euclidean', linkage = 'ward')
+y_hc = hc.fit_predict(X)
+ 
   
 # Visualising the clusters
 plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Cluster 1')
