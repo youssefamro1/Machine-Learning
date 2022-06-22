db3385f9bbe52fd9f853b18d6acca60965e83383
youssef amr momtaz <77958872+youssefamro1@users.noreply.github.com>
2022-06-22 06:32:03 +0200

    Update hierarchical_clustering.py

--- hierarchical_clustering.py
+++ hierarchical_clustering.py
@@ -8,7 +8,8 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Mall_Customers.csv')
 X = dataset.iloc[:, [3, 4]].values
-
+           
+ 
 # Using the dendrogram to find the optimal number of clusters
 import scipy.cluster.hierarchy as sch
 dendrogram = sch.dendrogram(sch.linkage(X, method = 'ward'))
